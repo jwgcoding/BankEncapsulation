12e34e50cafe6cea77aba35c76f510343c88dbf9
Gary Li <garyli0802@yahoo.com>
2022-09-23 12:08:59 -0700

    initial commit

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//In your BankAccount class, create the following:
+//A private field of type double named balance with a value of 0
+//Define a method named Deposit that will accept a double and store that value in the balance field
+//Define a method named GetBalance that will return the amount stored in the balance field
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+        public BankAccount()
+        {
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        } 
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+       
+    }
+}
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+//name it BankEncapsulation
+//Create a new public class named BankAccount
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using BankEncapsulation;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.WriteLine("What is the amount you are depositing?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The amount you are deposting is {amountToDeposit}.");
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your new balance is now {account.GetBalance()}");
+
+            Console.WriteLine("Enter an amount to withdraw?");
+            
+
+            var amountToWithdraw = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The amount you are withdrawing is {amountToWithdraw}.");
+            account.Withdraw(amountToWithdraw);
+            Console.WriteLine($"Thank you! Your new balance is now {account.GetBalance()}");
+
+
+
+
+
+        }
+
+
+    }
+
+}
